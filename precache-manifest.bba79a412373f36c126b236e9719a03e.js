3287f7c010e1d170cb1c3d9c3c244e2828baa7cd
doctoron <ron.antonio@me.com>
2020-03-22 09:15:12 -0400

    Updates

---
+++ precache-manifest.bba79a412373f36c126b236e9719a03e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9489f3d0c81158bc897c4e5e4640d1",
+    "url": "/am-portfolio/index.html"
+  },
+  {
+    "revision": "657be8135237e985d4f3",
+    "url": "/am-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "80e786871f32332a4aa4",
+    "url": "/am-portfolio/static/js/2.784f6427.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/am-portfolio/static/js/2.784f6427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657be8135237e985d4f3",
+    "url": "/am-portfolio/static/js/main.d545910b.chunk.js"
+  },
+  {
+    "revision": "d341aa850f305bfa8dde",
+    "url": "/am-portfolio/static/js/runtime-main.e9aa3c46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/am-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
