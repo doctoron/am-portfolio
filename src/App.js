cce9998037c43a98fc70de4947463da7077ed674
doctoron <ron.antonio@me.com>
2020-03-22 09:36:38 -0400

    transferred original src

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Portfolio from './pages/Portfolio';
+import About from './pages/Aboutme';
+import Contact from './pages/Contact';
+import Error from './components/Error';
+import Nav from './components/Nav';
+import Resume from './pages/Resume';
+import Footer from './components/Footer';
+import Images from './components/Images';
+// import TechModal from './components/TechModal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <Router basename="/">
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Portfolio} />
+            <Route path="/my-portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/my-resume" component={Resume} />
+            <Route path="/about" component={About} />
+            <Route path="/images" component={Images} />
+            <Route component={Error} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
